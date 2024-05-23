7d8ea8a3a3d29dfa383d8b2fbf2ed9c4c7e681cc
Dolores Polito <dolores.polito@gmail.com>
2024-05-23 12:53:52 -0300

    nav desktoplogo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,43 @@
-"use client"
+"use client";
 
 import { useTranslation } from "react-i18next";
 import styles from "./style.module.css";
 import Link from "next/link";
 import LanguageChanger from "@/components/LanguageChanger";
+import Image from "next/image";
+import logo from "../../../public/assets/logo-name.png"
 
 export default function Index() {
-
-const {t} = useTranslation('common');
+  const { t } = useTranslation("common");
   return (
     <div className={styles.section}>
-        <div className={styles.container}>
+      <div className={styles.container}>
+        <div className={styles.logocontainer}>
 
-            <Link href="/"><p className={styles.text}>{t('common:nav_home')}</p></Link>
-            <Link href="/about"><p className={styles.text}>{t('common:nav_about')}</p></Link>
-            <Link href="/procedures"><p className={styles.text}>{t('common:nav_procedures')}</p></Link>
-            <Link href="/space"><p className={styles.text}>{t('common:nav_space')}</p></Link>
-            <Link href="/testimonials"><p className={styles.text}>{t('common:nav_testimonials')}</p></Link>
-            <Link href="/contact"><p className={styles.text}>{t('common:nav_contact')}</p></Link>
-            <LanguageChanger/>
+          <Image src={logo} alt="image" className={styles.logo}/>
         </div>
-      
+        <div className={styles.linkscontainer}>
+          <Link href="/">
+            <p className={styles.text}>{t("common:nav_home")}</p>
+          </Link>
+          <Link href="/about">
+            <p className={styles.text}>{t("common:nav_about")}</p>
+          </Link>
+          <Link href="/procedures">
+            <p className={styles.text}>{t("common:nav_procedures")}</p>
+          </Link>
+          <Link href="/space">
+            <p className={styles.text}>{t("common:nav_space")}</p>
+          </Link>
+          <Link href="/testimonials">
+            <p className={styles.text}>{t("common:nav_testimonials")}</p>
+          </Link>
+          <Link href="/contact">
+            <p className={styles.text}>{t("common:nav_contact")}</p>
+          </Link>
+          <LanguageChanger />
+        </div>
+      </div>
     </div>
   );
 }
